25774d0ddce678b241a16104305a4c3b46376704
simone taddei <simone taddei>
2016-05-03 11:51:52 +0200

    First commit

---
+++ Gruppo AZ Tamagotchi/IOUtils/IOUtils.java
@@ -0,0 +1,244 @@
+package IOUtils;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class IOUtils {
+	
+	private static Scanner scanner = new Scanner(System.in);
+	
+	private final static String ERRORE_FORMATO = "Attenzione: il dato inserito non e' nel formato corretto";
+	  private final static String ERRORE_MINIMO= "Attenzione: e' richiesto un valore maggiore o uguale a ";
+	  private final static String ERRORE_STRINGA_VUOTA= "Attenzione: non hai inserito alcun carattere";
+	  private final static String ERRORE_MASSIMO= "Attenzione: e' richiesto un valore minore o uguale a ";
+	  private final static String MESSAGGIO_AMMISSIBILI= "Attenzione: i caratteri ammissibili sono: ";
+
+	  private final static char RISPOSTA_SI='S';
+	  private final static char RISPOSTA_NO='N';
+	  
+	  public static String leggiStringa (String messaggio)
+	  {
+		  System.out.print(messaggio);
+		  return scanner.next();
+	  }
+	  
+	  public static String leggiStringaNonVuota(String messaggio)
+	  {
+	   boolean finito=false;
+	   String lettura = null;
+	   do
+	   {
+		 lettura = leggiStringa(messaggio);
+		 lettura = lettura.trim();
+		 if (lettura.length() > 0)
+		  finito=true;
+		 else
+		  System.out.println(ERRORE_STRINGA_VUOTA);
+	   } while (!finito);
+	   
+	   return lettura;
+	  }
+	  
+	  public static char leggiChar (String messaggio)
+	  {
+	   boolean finito = false;
+	   char valoreLetto = '\0';
+	   do
+	    {
+	     System.out.print(messaggio);
+	     String lettura = scanner.next();
+	     if (lettura.length() > 0)
+	      {
+	       valoreLetto = lettura.charAt(0);
+	       finito = true;
+	      }
+	     else
+	      {
+	       System.out.println(ERRORE_STRINGA_VUOTA);
+	      }
+	    } while (!finito);
+	   return valoreLetto;
+	  }
+	  
+	  public static char leggiUpperChar (String messaggio, String ammissibili)
+	  {
+	   boolean finito = false;
+	   char valoreLetto = '\0';
+	   do
+	   {
+	    valoreLetto = leggiChar(messaggio);
+	    valoreLetto = Character.toUpperCase(valoreLetto);
+	    if (ammissibili.indexOf(valoreLetto) != -1)
+		 finito  = true;
+	    else
+	     System.out.println(MESSAGGIO_AMMISSIBILI + ammissibili);
+	   } while (!finito);
+	   return valoreLetto;
+	  }
+	  
+	  
+	  public static int leggiIntero (String messaggio)
+	  {
+	   boolean finito = false;
+	   int valoreLetto = 0;
+	   do
+	    {
+	     System.out.print(messaggio);
+	     if (scanner.hasNextInt())
+	      {
+	       valoreLetto = scanner.nextInt();
+	       finito = true;
+	      }
+	     else
+	      {
+	       System.out.println(ERRORE_FORMATO);
+	       String daButtare = scanner.next();
+	      }
+	    } while (!finito);
+	   return valoreLetto;
+	  }
+
+	  public static int leggiInteroPositivo(String messaggio)
+	  {
+		  return leggiInteroConMinimo(messaggio,1);
+	  }
+	  
+	  public static int leggiInteroNonNegativo(String messaggio)
+	  {
+		  return leggiInteroConMinimo(messaggio,0);
+	  }
+	  
+	  
+	  public static int leggiInteroConMinimo(String messaggio, int minimo)
+	  {
+	   boolean finito = false;
+	   int valoreLetto = 0;
+	   do
+	    {
+	     valoreLetto = leggiIntero(messaggio);
+	     if (valoreLetto >= minimo)
+	      finito = true;
+	     else
+	      System.out.println(ERRORE_MINIMO + minimo);
+	    } while (!finito);
+	     
+	   return valoreLetto;
+	  }
+
+	  public static int leggiIntero(String messaggio, int minimo, int massimo)
+	  {
+	   boolean finito = false;
+	   int valoreLetto = 0;
+	   do
+	    {
+	     valoreLetto = leggiIntero(messaggio);
+	     if (valoreLetto >= minimo && valoreLetto<= massimo)
+	      finito = true;
+	     else
+	      if (valoreLetto < minimo)
+	         System.out.println(ERRORE_MINIMO + minimo);
+	      else
+	    	 System.out.println(ERRORE_MASSIMO + massimo); 
+	    } while (!finito);
+	     
+	   return valoreLetto;
+	  }
+
+	  
+	  public static double leggiDouble (String messaggio)
+	  {
+	   boolean finito = false;
+	   double valoreLetto = 0;
+	   do
+	    {
+	     System.out.print(messaggio);
+	     if (scanner.hasNextDouble())
+	      {
+	       valoreLetto = scanner.nextDouble();
+	       finito = true;
+	      }
+	     else
+	      {
+	       System.out.println(ERRORE_FORMATO);
+	       String daButtare = scanner.next();
+	      }
+	    } while (!finito);
+	   return valoreLetto;
+	  }
+	 
+	  public static double leggiDoubleConMinimo (String messaggio, double minimo)
+	  {
+	   boolean finito = false;
+	   double valoreLetto = 0;
+	   do
+	    {
+	     valoreLetto = leggiDouble(messaggio);
+	     if (valoreLetto >= minimo)
+	      finito = true;
+	     else
+	      System.out.println(ERRORE_MINIMO + minimo);
+	    } while (!finito);
+	     
+	   return valoreLetto;
+	  }
+
+	  
+	  public static boolean yesOrNo(String messaggio)
+	  {
+		  String mioMessaggio = messaggio + "("+RISPOSTA_SI+"/"+RISPOSTA_NO+")";
+		  char valoreLetto = leggiUpperChar(mioMessaggio,String.valueOf(RISPOSTA_SI)+String.valueOf(RISPOSTA_NO));
+		  
+		  if (valoreLetto == RISPOSTA_SI)
+			return true;
+		  else
+			return false;
+	  }
+	  
+	public static int GetInt()
+	{
+		return scanner.nextInt();
+	}
+	
+	
+	public static int estraiIntero(int min, int max)
+	{
+		 Random estrattore = new Random();
+		 
+		 int range = max + 1 - min;
+		 int casual = estrattore.nextInt(range);
+		 return casual + min;
+	}
+	
+	public static int media(int numeri[],int n)
+	{
+		int somma=0;
+		//int n=numeri.
+		for(int i=0;i<n;i++)
+		{
+			somma+=numeri[i];
+		}
+		return somma/n;
+	}
+	
+	public static float media(float numeri[], int n)
+	{
+		float somma=0;
+		for(int i=0;i<n;i++)
+		{
+			somma+=numeri[i];
+		}
+		return somma/n;
+	}
+	
+	public static int menu(String voci[])
+	{
+		for(int i=0;i<voci.length;i++)
+		{
+			System.out.println("\n"+(i+1)+") "+voci[i]);
+			
+		}
+		System.out.println("\n Inserire la tua scelta: ");
+		return GetInt();
+	}
+	
+}
